05c7022a8b8a12bee7a73f3bd95223d8eb1f6e8f
Colevo <algonz22@ucm.es>
2024-12-20 23:55:35 +0100

    Nav

--- webnacar/src/App.jsx
+++ webnacar/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Nav from "./components/nav";
+import Nav from "./components/Nav"; 
 import About from "./pages/About";
 import Conocenos from "./pages/Conoce";
 import Inscripcion from "./pages/Inscripcion";
